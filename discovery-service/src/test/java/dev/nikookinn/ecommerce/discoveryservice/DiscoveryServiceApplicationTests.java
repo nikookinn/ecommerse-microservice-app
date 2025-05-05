b5724168cf4fdd669114ff1437dd90f1474c8167
skywalker <nikookinn@hotmail.com>
2025-05-05 15:50:54 +0400

    EA-9 discovery service yml file configured in config server

---
+++ discovery-service/src/test/java/dev/nikookinn/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nikookinn.ecommerce.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
